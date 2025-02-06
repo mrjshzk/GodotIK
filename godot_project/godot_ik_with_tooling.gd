adae505f82df2d8d13b974286f60debcd36228f6
monxa <84077629+monxa@users.noreply.github.com>
2025-02-06 07:40:14 +0100

    Adjust project for godot 4.3 (#20)

--- godot_project/godot_ik_with_tooling.gd
+++ godot_project/godot_ik_with_tooling.gd
@@ -1,10 +1,11 @@
 @tool
 class_name GodotIKWithTooling extends GodotIK
 
-@export_tool_button("Reset all effectors") var t = func():
-	var skeleton = get_skeleton()
-	skeleton.reset_bone_poses()
-
-	for effector : GodotIKEffector in get_children():
-		var trans = skeleton.global_transform * skeleton.get_bone_global_pose(effector.bone_idx)
-		effector.global_transform = trans
+## This only works with godot 4.4+ WE NEED SOME EditorPlugin to provide tooling, please <3
+#@export_tool_button("Reset all effectors") var t = func():
+	#var skeleton = get_skeleton()
+	#skeleton.reset_bone_poses()
+#
+	#for effector : GodotIKEffector in get_children():
+		#var trans = skeleton.global_transform * skeleton.get_bone_global_pose(effector.bone_idx)
+		#effector.global_transform = trans
